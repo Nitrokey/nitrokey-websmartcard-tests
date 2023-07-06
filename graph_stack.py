d37c58eaa79db1dfaf66d9809c9112a45b7df0a1
Szczepan Zalega <szczepan@nitrokey.com>
2023-07-06 15:28:18 +0200

    Make graph script a better CLI tool (needs Fire now)
    Filters required data by itself (no need to pre-grep)
    Names insteresting samples for the furter debugging

--- graph_stack.py
+++ graph_stack.py
@@ -1,16 +1,31 @@
 from matplotlib import pyplot as plt
+import fire
 
-path = 'gdb.txt.stack2'
 
-
-def main():
+def main(path, threshold=50):
     data = []
+    last_line = []
     with open(path, 'r') as f:
-        for line in f:
-            num = int(line.split()[-1], 16)
+        for i, line in enumerate(f):
+            if "(*mut ()) 0x" not in line:
+                last_line.append(line)
+                continue
+            try:
+                num = int(line.split()[-1], 16)
+            except Exception:
+                print(f'Skipping "{line}"')
+                continue
+
             num = num - 0x20000000
             num = num // 1024
+            if num < 0 or num > 1*1024*1024:
+                continue
             data.append(num)
+
+            if num < threshold:
+                print((i,len(data),num,last_line))
+            last_line.clear()
+
 
     print(len(data))
     plt.plot(data)
@@ -20,4 +35,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    fire.Fire()
